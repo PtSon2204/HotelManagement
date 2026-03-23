d7c2e72b264dd94d1a2a6df501745ec9ca8a8b55
Huy Nguyen <nguyenhuyquang1096@gmail.com>
2026-03-24 01:15:38 +0700

    app setting

--- HotelManagement/HotelManagement/Program.cs
+++ HotelManagement/HotelManagement/Program.cs
@@ -17,7 +17,7 @@ namespace HotelManagement
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // 1. Thêm Cookie Authentication (Bắt buộc cho SignalR nhận diện User)
+            // 1. Thêm Cookie Authentication (Từ Code 2)
             builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
                 .AddCookie(options =>
                 {
@@ -25,20 +25,21 @@ namespace HotelManagement
                     options.AccessDeniedPath = "/Account/AccessDenied";
                 });
 
-            // 2. Thêm SignalR và Provider nhận diện ID
+            // 2. Thêm SignalR (Từ Code 2)
             builder.Services.AddSignalR();
             builder.Services.AddSingleton<IUserIdProvider, CustomUserIdProvider>();
 
-            // Add services to the container.
+            // 3. Cấu hình MVC & Filters
             builder.Services.AddControllersWithViews(options =>
             {
                 options.Conventions.Add(new AdminAreaConvention());
             });
 
+            // 4. Cấu hình Database
             builder.Services.AddDbContext<ApplicationDbContext>(options =>
                options.UseSqlServer(builder.Configuration.GetConnectionString("MyCnn")));
 
-            // Đăng ký các Repository và Service
+            // 5. Đăng ký các Repository và Service (Hợp nhất từ cả 2 bản)
             builder.Services.AddScoped<BookingServiceHanlde>();
             builder.Services.AddScoped<BookingRepository>();
             builder.Services.AddScoped<CustomerRepository>();
@@ -63,46 +64,54 @@ namespace HotelManagement
 
             var app = builder.Build();
 
-            // Seed Data (Giữ nguyên logic của bạn)
+            // 6. Seed Data (Giữ logic tạo bảng Messages của Code 2 và đủ User của Code 1)
             using (var scope = app.Services.CreateScope())
             {
                 var context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+
+                // Tạo bảng Messages nếu chưa có
                 context.Database.ExecuteSqlRaw(@"
-IF OBJECT_ID(N'dbo.Messages', N'U') IS NULL
-BEGIN
-    CREATE TABLE [dbo].[Messages](
-        [MessageId] [int] IDENTITY(1,1) NOT NULL PRIMARY KEY,
-        [SenderName] [nvarchar](100) NOT NULL,
-        [Content] [nvarchar](1000) NOT NULL,
-        [SentAt] [datetime] NOT NULL CONSTRAINT [DF_Messages_SentAt] DEFAULT (GETDATE())
-    );
-END
-");
+                    IF OBJECT_ID(N'dbo.Messages', N'U') IS NULL
+                    BEGIN
+                        CREATE TABLE [dbo].[Messages](
+                            [MessageId] [int] IDENTITY(1,1) NOT NULL PRIMARY KEY,
+                            [SenderName] [nvarchar](100) NOT NULL,
+                            [Content] [nvarchar](1000) NOT NULL,
+                            [SentAt] [datetime] NOT NULL CONSTRAINT [DF_Messages_SentAt] DEFAULT (GETDATE())
+                        );
+                    END");
+
                 if (!context.Users.Any())
                 {
+                    // Thêm Staff
                     var staff1 = new Staff { FullName = "Staff 1" };
                     var staff2 = new Staff { FullName = "Staff 2" };
                     context.Staffs.AddRange(staff1, staff2);
                     context.SaveChanges();
 
+                    // Thêm đầy đủ 3 Customer (Từ Code 1)
                     var c1 = new Customer { FullName = "Customer 1" };
                     var c2 = new Customer { FullName = "Customer 2" };
                     var c3 = new Customer { FullName = "Customer 3" };
                     context.Customers.AddRange(c1, c2, c3);
                     context.SaveChanges();
 
+                    // Thêm đầy đủ Users
                     var users = new List<User>
                     {
                         new User { Username = "admin", PasswordHash = Hash("123"), Role = "Admin" },
                         new User { Username = "staff1", PasswordHash = Hash("123"), Role = "Staff", StaffId = staff1.StaffId },
                         new User { Username = "staff2", PasswordHash = Hash("123"), Role = "Staff", StaffId = staff2.StaffId },
-                        new User { Username = "cus1", PasswordHash = Hash("123"), Role = "Customer", CustomerId = c1.CustomerId }
+                        new User { Username = "cus1", PasswordHash = Hash("123"), Role = "Customer", CustomerId = c1.CustomerId },
+                        new User { Username = "cus2", PasswordHash = Hash("123"), Role = "Customer", CustomerId = c2.CustomerId },
+                        new User { Username = "cus3", PasswordHash = Hash("123"), Role = "Customer", CustomerId = c3.CustomerId }
                     };
                     context.Users.AddRange(users);
                     context.SaveChanges();
                 }
             }
 
+            // 7. HTTP Pipeline
             if (!app.Environment.IsDevelopment())
             {
                 app.UseExceptionHandler("/Home/Error");
@@ -114,11 +123,10 @@ END
             app.UseSession();
             app.UseRouting();
 
-            // QUAN TRỌNG: Authentication phải đứng trước Authorization
+            // Thứ tự quan trọng: Auth trước, SignalR Hub sau
             app.UseAuthentication();
             app.UseAuthorization();
 
-            // Map SignalR Hub
             app.MapHub<ChatHub>("/chatHub");
 
             app.MapControllerRoute(
@@ -132,7 +140,6 @@ END
             app.Run();
         }
 
-        // Sửa lỗi 'not all code paths return a value'
         public static string Hash(string password)
         {
             using var sha256 = System.Security.Cryptography.SHA256.Create();
